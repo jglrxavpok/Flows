0029e53c6871ebb96ca203e556cf1940da6b7c06
xavpok <jglrxavpok@gmail.com>
2015-12-23 14:43:27 +0100

    Added InputStream created from URLs to the testing

--- src/test/java/LoopingTests.java
+++ src/test/java/LoopingTests.java
@@ -2,9 +2,12 @@ import org.jglr.flows.looping.LoopingInputStream;
 import org.jglr.flows.looping.StreamLoop;
 import org.jglr.flows.looping.defaults.DoStreamLoop;
 import org.jglr.flows.looping.defaults.RepeatStreamLoop;
+import org.jglr.flows.looping.defaults.WhileStreamLoop;
 import org.junit.Test;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.function.Function;
 
 import static org.junit.Assert.assertThat;
@@ -13,12 +16,14 @@ import static org.junit.Assert.assertTrue;
 public class LoopingTests {
 
     @Test
-    public void instantiationTests() throws FileNotFoundException {
+    public void instantiationTests() throws IOException {
         testWithBuffering(getClass().getResourceAsStream("/test.txt"), "classpath");
         testWithBuffering(new FileInputStream(new File(".", "build.gradle")), "file");
+        testWithBuffering(new URL("http://google.com").openStream(), "http");
 
         testWithNoBuffering(getClass().getResourceAsStream("/test.txt"), "classpath");
         testWithNoBuffering(new FileInputStream(new File(".", "build.gradle")), "file");
+        testWithNoBuffering(new URL("http://google.com").openStream(), "http");
     }
 
     private void testWithBuffering(InputStream in, String name) {
@@ -41,6 +46,16 @@ public class LoopingTests {
     @Test
     public void testDefaultLoops() throws IOException {
         testLoops(true, (a) -> getClass().getResourceAsStream("/test.txt"), "classpath");
+        testLoops(false, (a) -> getClass().getResourceAsStream("/test.txt"), "classpath");
+
+        testLoops(true, (a) -> {
+            try {
+                return new FileInputStream(new File(".", "build.gradle"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }, "file");
         testLoops(false, (a) -> {
             try {
                 return new FileInputStream(new File(".", "build.gradle"));
@@ -49,12 +64,31 @@ public class LoopingTests {
                 return null;
             }
         }, "file");
+
+        testLoops(true, (a) -> {
+            try {
+                return new URL("http://google.com").openStream();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }, "http");
+        testLoops(false, (a) -> {
+            try {
+                return new URL("http://google.com").openStream();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }, "http");
     }
 
     private void testLoops(boolean buffering, Function<Void, InputStream> in, String name) throws IOException {
         // Test do loop
         testLoop(buffering, in.apply(null), name, new DoStreamLoop(0, Long.MAX_VALUE));
         testLoop(buffering, in.apply(null), name, new RepeatStreamLoop(0, Long.MAX_VALUE, 5));
+
+        testLoop(buffering, in.apply(null), name, new WhileStreamLoop(0, Long.MAX_VALUE, c -> c < 20));
     }
 
     private void testLoop(boolean buffering, InputStream in, String name, StreamLoop loop) throws IOException {
